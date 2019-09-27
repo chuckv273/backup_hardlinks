a550c436437acb74d146dbcfa7a8ad507fc5b746
Chuck Villa <chuck@chuckvilla.org>
2019-09-27 00:08:22 -0700

    Added config file with multiple source directories. Now I can use the tool for daily internal backups.

--- backup.py
+++ backup.py
@@ -8,6 +8,7 @@ import subprocess
 import win32api
 import win32con
 import sys
+import yaml
 import argparse
 
 
@@ -29,8 +30,8 @@ def check_file_info(file_infos, always_check_hash):
     for i in range(len(file_infos)):
         try:
             sr = os.stat(file_infos[i][0])
-            file_changed = ((sr.st_size != int(file_infos[i][2])) or (sr.st_mtime_ns != int(file_infos[i][3])) or \
-                    (sr.st_ctime_ns != int(file_infos[i][4])))
+            file_changed = ((sr.st_size != int(file_infos[i][2])) or (sr.st_mtime_ns != int(file_infos[i][3])) or
+                           (sr.st_ctime_ns != int(file_infos[i][4])))
             if file_changed or always_check_hash:
                 hash_val = hash_file(file_infos[i][0])
                 if file_changed or (hash_val != file_infos[i][1]):
@@ -138,86 +139,112 @@ def copy_mailbox(backup_dir):
     return file_count, file_size
 
 
-def do_backup(backup_dir, check_g_hashes):
+def do_backup(backup_dir, sources, dest_hash_csv, source_hash_csv, check_dest_hashes):
+    """
+    :param backup_dir: str: destination directory for backup
+    :param sources: list of source paths. All sub dirs are included
+    :param dest_hash_csv: csv file with hashes on destination volume
+    :param source_hash_csv: csv file with hashes on source volume
+    :param check_dest_hashes: bool: if true, rehashes files on dest volume to verify hashes
+    :return:
+    """
     hash_targets = {}
     hash_sources = {}
-    print('Checking G:\ hashes')
-    populate_hash_dict(hash_targets, 'C:\\Backup\\g-hash.csv', check_g_hashes)
-    print('Checking C:\ hashes')
-    populate_name_dict(hash_sources, 'C:\\Backup\\c-hash.csv')
+    print('Checking dest hashes')
+    populate_hash_dict(hash_targets, dest_hash_csv, check_dest_hashes)
+    print('Checking source hashes')
+    populate_name_dict(hash_sources, source_hash_csv)
     new_bytes = 0
-    source_dir = 'C:\\Users\\jen-chuck\\OneDrive'
     print('Executing backup')
     new_files = 0
-    for (dpath, dnames, fnames) in os.walk(source_dir):
-        dest_dir = os.path.join(backup_dir, os.path.relpath(dpath, source_dir))
-        os.makedirs(dest_dir, exist_ok=True)
-        if dpath.count('\\') <= 4:
-            print(dpath)
-        # Only take latest file from Parity server backups
-        if dpath == 'C:\\Users\\jen-chuck\\OneDrive\\Parity':
-            lastest_time = 0
-            latest_name = None
+    for source_dir in sources:
+        for (dpath, dnames, fnames) in os.walk(source_dir):
+            drive, path = os.path.splitdrive(dpath)
+            if path[0]=='\\':
+                path = path[1:]
+            dest_dir = os.path.join(backup_dir, path)
+            os.makedirs(dest_dir, exist_ok=True)
+            if dpath.count('\\') <= 4:
+                print(dpath)
+            # Only take latest file from Parity server backups
+            if dpath == 'C:\\Users\\jen-chuck\\OneDrive\\Parity':
+                lastest_time = 0
+                latest_name = None
+                for file_name in fnames:
+                    sr = os.stat(os.path.join(dpath, file_name))
+                    if sr.st_mtime_ns > lastest_time:
+                        lastest_time = sr.st_mtime_ns
+                        latest_name = file_name
+                fnames = [latest_name]
             for file_name in fnames:
-                sr = os.stat(os.path.join(dpath, file_name))
-                if sr.st_mtime_ns > lastest_time:
-                    lastest_time = sr.st_mtime_ns
-                    latest_name = file_name
-            fnames = [latest_name]
-        for file_name in fnames:
-            try:
-                file_path = os.path.join(dpath, file_name)
-                sr = os.stat(file_path)
-                if file_path in hash_sources:
-                    info = hash_sources[file_path]
-                else:
-                    info = None
-                if info and (sr.st_size == int(info[2])) and (sr.st_mtime_ns == int(info[3])) and \
-                        (sr.st_ctime_ns == int(info[4])):
-                    hash_val = info[1]
-                else:
-                    print('Hashing {}'.format(file_path))
-                    hash_val = hash_file(file_path)
-                    hash_sources[file_path] = [file_path, hash_val, sr.st_size, sr.st_mtime_ns, sr.st_ctime_ns]
-                rel_path = os.path.relpath(file_path, source_dir)
-                dest_path = os.path.join(backup_dir, rel_path)
-                use_copy = True
-                if hash_val in hash_targets:
-                    # make link
-                    # print("Link {} to {}".format(dest_path, hash_targets[hash][0]))
-                    try:
-                        os.link(hash_targets[hash_val][0], dest_path)
-                        use_copy = False
-                    except Exception:
-                        pass
-                if use_copy:
-                    # copy new file
-                    print('new file {}'.format(file_path))
-                    shutil.copy2(file_path, dest_path)
-                    sr = os.stat(dest_path)
-                    new_bytes += sr.st_size
-                    hash_targets[hash_val] = [dest_path, hash_val, sr.st_size, sr.st_mtime_ns, sr.st_ctime_ns]
-                    new_files += 1
-                    win32api.SetFileAttributes(dest_path, win32con.FILE_ATTRIBUTE_READONLY)
-            except Exception:
-                pass
+                try:
+                    file_path = os.path.join(dpath, file_name)
+                    sr = os.stat(file_path)
+                    if file_path in hash_sources:
+                        info = hash_sources[file_path]
+                    else:
+                        info = None
+                    if info and (sr.st_size == int(info[2])) and (sr.st_mtime_ns == int(info[3])) and \
+                            (sr.st_ctime_ns == int(info[4])):
+                        hash_val = info[1]
+                    else:
+                        print('Hashing {}'.format(file_path))
+                        hash_val = hash_file(file_path)
+                        hash_sources[file_path] = [file_path, hash_val, sr.st_size, sr.st_mtime_ns, sr.st_ctime_ns]
+                    drive, path = os.path.splitdrive(file_path)
+                    path = path[1:]
+                    dest_path = os.path.join(backup_dir, path)
+                    use_copy = True
+                    if hash_val in hash_targets:
+                        # make link
+                        # print("Link {} to {}".format(dest_path, hash_targets[hash][0]))
+                        try:
+                            os.link(hash_targets[hash_val][0], dest_path)
+                            use_copy = False
+                        except Exception:
+                            pass
+                    if use_copy:
+                        # copy new file
+                        print('new file {}'.format(file_path))
+                        shutil.copy2(file_path, dest_path)
+                        sr = os.stat(dest_path)
+                        new_bytes += sr.st_size
+                        hash_targets[hash_val] = [dest_path, hash_val, sr.st_size, sr.st_mtime_ns, sr.st_ctime_ns]
+                        new_files += 1
+                        win32api.SetFileAttributes(dest_path, win32con.FILE_ATTRIBUTE_READONLY)
+                except Exception:
+                    pass
     write_file_infos(hash_targets, 'C:\\Backup\\g-hash.csv')
     write_file_infos(hash_sources, 'C:\\Backup\\c-hash.csv')
     for hash_name in ['g-hash.csv', 'c-hash.csv']:
         shutil.copy2(os.path.join('C:\\Backup', hash_name), os.path.join(backup_dir, hash_name))
     return new_files, new_bytes
 
-parser = argparse.ArgumentParser(description='Backup with hardlinks')
-check_g_hashes = (len(sys.argv) > 1 and sys.argv[1]=='/check-hashes')
-backup_dir = os.path.join('G:\\Backups', date.today().strftime('%Y-%m-%d'))
-os.makedirs(backup_dir, exist_ok=True)
-new_files = 0
-new_bytes = 0
-counts = copy_mailbox(backup_dir)
-new_files += counts[0]
-new_bytes += counts[1]
-counts = do_backup(backup_dir, check_g_hashes)
-new_files += counts[0]
-new_bytes += counts[1]
 
-print('New files: {:,}\nbytes: {:,}'.format(new_files, new_bytes))
+parser = argparse.ArgumentParser(description='Backup with hardlinks')
+parser.add_argument('config_file', help='Path to configuration yaml file')
+parser.add_argument('--check-hashes', help='Verify stored destination hashes. Can add a lot of time to the backup.',
+                    action='store_true')
+args = parser.parse_args()
+
+if args.config_file:
+    config = {}
+    with open(args.config_file, 'r') as stream:
+        config = yaml.safe_load(stream)
+    if 'dest' in config and 'sources' in config and 'dest_hashes' in config and 'source_hashes' in config:
+        backup_dir = os.path.join(config['dest'], date.today().strftime('%Y-%m-%d'))
+        os.makedirs(backup_dir, exist_ok=True)
+        new_files = 0
+        new_bytes = 0
+        counts = copy_mailbox(backup_dir)
+        new_files += counts[0]
+        new_bytes += counts[1]
+        counts = do_backup(backup_dir, config['sources'], config['dest_hashes'], config['source_hashes'],
+                           args.check_hashes)
+        new_files += counts[0]
+        new_bytes += counts[1]
+        print('New files: {:,}\nbytes: {:,}'.format(new_files, new_bytes))
+    else:
+        print('Config file missing required values. No backup.')
+else:
+    print('No config file specified.')
